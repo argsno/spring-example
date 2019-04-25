77211076eaaef915937be092a5b51d120b1cc5f9
黄志强 <huangzhiqiang02@meituan.com>
2019-04-25 16:37:40 +0800

    add

---
+++ spring-boot-commandlinerunner/src/main/java/com/argsno/springbootcommandlinerunner/SpringBootCommandlinerunnerApplication.java
@@ -0,0 +1,13 @@
+package com.argsno.springbootcommandlinerunner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCommandlinerunnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCommandlinerunnerApplication.class, args);
+	}
+
+}
